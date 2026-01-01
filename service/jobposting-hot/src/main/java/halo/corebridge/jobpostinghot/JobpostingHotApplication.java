fcb66090a38f42e2584becfda6c0ca69fa07007b
atimaby28 <atimaby28@gmail.com>
2026-01-01 19:58:08 +0900

    Initial commit

---
+++ service/jobposting-hot/src/main/java/halo/corebridge/jobpostinghot/JobpostingHotApplication.java
@@ -0,0 +1,11 @@
+package halo.corebridge.jobpostinghot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpostingHotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobpostingHotApplication.class, args);
+    }
+}
---
+++ service/jobposting-read/src/main/java/halo/corebridge/jobpostingread/JobpostingReadApplication.java
@@ -0,0 +1,11 @@
+package halo.corebridge.jobpostingread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpostingReadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobpostingReadApplication.class, args);
+    }
+}
